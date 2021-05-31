baff23f2d3b3cfaeebf2c6dfbca882dea6341451
Zoran Milosevic <zoran.milosevic@outlook.com>
2021-05-31 20:04:59 +0200

    update

--- src/frontend/client/src/app/app-routing.module.ts
+++ src/frontend/client/src/app/app-routing.module.ts
@@ -3,9 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     { path: '', redirectTo: 'database', pathMatch: 'full' },
-    { path: 'database', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
-    { path: 'file', loadChildren: () => import('./modules/file/file.module').then(m => m.FileModule) },
-    { path: 'user-input', loadChildren: () => import('./modules/user-input/user-input.module').then(m => m.UserInputModule) }
+    { path: 'database', loadChildren: () => import('@feature/home/home.module').then(m => m.HomeModule) },
+    { path: 'file', loadChildren: () => import('@feature/file/file.module').then(m => m.FileModule) },
+    { path: 'user-input', loadChildren: () => import('@feature/user-input/user-input.module').then(m => m.UserInputModule) }
 ];
 
 @NgModule({
